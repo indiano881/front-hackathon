80dd773086584a780b67c27077869be65c4b5750
Davide Baldi <d.baldi88@gmail.com>
2024-07-03 12:33:31 +0200

    save progress-need to continue from footer hero

---
+++ components/HeroFooter/page.tsx
@@ -0,0 +1,11 @@
+
+
+const HeroFooter = () => {
+    return (
+        <div>
+          
+        </div>
+    )
+};
+
+export default HeroFooter
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Hero from "../../components/Hero/page";
 
 export default function Home() {
   return (
     <main className="">
       <h1 className="text-white">Hello world</h1>
+      <Hero />
     </main>
   );
 }
