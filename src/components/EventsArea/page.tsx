7a20657badb7fd5dd6c7dbe94e0a094ad6c910ba
Davide Baldi <d.baldi88@gmail.com>
2024-07-07 17:36:52 +0200

    working on the image typescript prop

--- src/components/EventsArea/page.tsx
+++ src/components/EventsArea/page.tsx
@@ -9,8 +9,11 @@ const EventsArea = () => {
         <div className="h-full">
             <BigTitle text1={"FUTURE"} text2={"EVENTS"} />
             <UnderTitleText text={"Check out the upcoming and similar events! Coding is fun! Running an Hackathon is amazing!"} />
-            <EventsCard image="../../../public/event1.jpeg" />
-            <EventsCard image="../../../public/ai.jpg" />
+            <div className="flex flex-row justify-evenly m-6">
+                <EventsCard  />
+                <EventsCard  />
+                <EventsCard  />
+            </div>
         </div>
     )
 };
