bd0f2054a2fd95820e9d9219a91bae6e98abddbd
Davide Baldi <d.baldi88@gmail.com>
2024-07-07 09:53:00 +0200

    added new images

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+
+
+const about = () => {
+    return (
+        <div>
+          <p>about us</p>
+          <p>Jury</p>
+          <p>Speakers</p>
+          <p>Staff</p>
+        </div>
+    )
+};
+
+export default about
