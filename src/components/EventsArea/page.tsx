4752f08023cdf9b64f4f2abc861c52704f09dabf
Davide Baldi <d.baldi88@gmail.com>
2024-07-07 17:10:37 +0200

    save2

--- src/components/EventsArea/page.tsx
+++ src/components/EventsArea/page.tsx
@@ -1,13 +1,16 @@
 import BigTitle from "../BigTitle/page";
+import EventsCard from "../EventsCard/page";
 import UnderTitleText from "../UnderTitleText/page";
-
+import Event1 from "../../../public/event1.jpeg"
+import Event2 from "../../../public/ai.jpg"
 
 const EventsArea = () => {
     return (
         <div className="h-full">
             <BigTitle text1={"FUTURE"} text2={"EVENTS"} />
             <UnderTitleText text={"Check out the upcoming and similar events! Coding is fun! Running an Hackathon is amazing!"} />
-          
+            <EventsCard image="../../../public/event1.jpeg" />
+            <EventsCard image="../../../public/ai.jpg" />
         </div>
     )
 };
