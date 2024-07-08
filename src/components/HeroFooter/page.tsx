50f0da3f16555c909bd900297f6e28fcb4562c36
Davide Baldi <d.baldi88@gmail.com>
2024-07-08 17:09:38 +0200

    making responsive website

--- src/components/HeroFooter/page.tsx
+++ src/components/HeroFooter/page.tsx
@@ -2,9 +2,9 @@
 
 const HeroFooter = () => {
     return (
-        <div className="bg-c-green">
-            <p className="p1 italic">Frontendackathon ´24 Frontendackathon ´24 Frontendackathon ´24 Frontendackathon ´24</p>
-          
+        <div className="bg-c-green text-center">
+            <p className="p1 italic hidden md:inline-block ">❤️Frontendackathon ´24 ❤️ Frontendackathon ´24 ❤️ Frontendackathon ´24 ❤️Frontendackathon ´24 ❤️</p>
+            <p className="h2 italic md:hidden">❤️Frontendackathon ´24❤️ </p>
         </div>
     )
 };
