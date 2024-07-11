10715e7347be53c656f854abc8fec12da8f4f77a
Davide Baldi <d.baldi88@gmail.com>
2024-07-11 21:19:44 +0200

    save progress cms in this branch

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,11 +1,39 @@
 
 
-const stories = () => {
-    return (
-        <div>
-          <p>stories</p>
-        </div>
-    )
-};
 
-export default stories
+// pages/index.js
+import { gql } from 'graphql-request';
+import { graphQLClient } from '../../../lib/graphql-client.js';
+
+
+const QUERY = gql`
+  {
+    posts {
+      title
+      slug
+      text_LONG
+      image
+    }
+  }
+`;
+
+
+
+export default async function stories() {
+  const { posts }:any = await graphQLClient.request(QUERY);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post:any) => (
+          <li>
+            <h2>{post.title}</h2>
+            <p>{post.text_LONG}</p>
+            <p><img src={post.image} alt={post.title} /></p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
