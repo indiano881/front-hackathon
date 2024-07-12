3011d57d2d4960b68ac8072fd18d057cebf97ffb
Davide Baldi <d.baldi88@gmail.com>
2024-07-12 10:32:18 +0200

    amazing achievemnt

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,11 +1,38 @@
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, AwaitedReactNode } from "react"
+import client from "../../../apolloClient"
+import {gql} from "@apollo/client"
 
-
+let dataCleaned:any
+export async function getData() {
+  
+  const {data} = await client.query({
+    query: gql`{davides{
+      title
+      slug
+      textLong
+      image {
+        id
+      }
+    }}` 
+  })
+  console.log(data.davides)
+  dataCleaned = data.davides;
+  return dataCleaned
+}
+getData()
 const stories = () => {
     return (
         <div>
           <p>stories</p>
+          {dataCleaned.map((item: any, index: any)=> <div>
+            <p> {item.title}</p>
+            <p> {item.textLong}</p>
+            <img src={item.image} alt={item.title} className=""/>
+            </div>)}
         </div>
     )
 };
 
 export default stories
+
+
