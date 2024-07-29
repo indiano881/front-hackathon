3f0f347b1bd95f8e6eaa11c0db76dac9ecc6782f
Davide Baldi <d.baldi88@gmail.com>
2024-07-29 11:48:40 +0200

    working on hamburger menu

--- src/components/Navbar/page.tsx
+++ src/components/Navbar/page.tsx
@@ -94,7 +94,7 @@ export default function NavbarC() {
     >
       <motion.svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
+        className="h-6 w-6 z-20 absolute right-2 bottom-6"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
