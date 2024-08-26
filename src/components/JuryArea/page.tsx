595f61c14d2c80bd07202efdec59e9946c9686ef
Davide Baldi <d.baldi88@gmail.com>
2024-08-26 07:15:36 +0200

    fixing jury part

--- src/components/JuryArea/page.tsx
+++ src/components/JuryArea/page.tsx
@@ -2,20 +2,21 @@ import BigTitle from "../BigTitle/page";
 import DinamicCard from "../DinamicCard";
 import nerd from "../../../public/nerd.png"
 const judgeArray = [
+  
   {
-      name: "Dr. Jane Smith",
-      title: "AI Researcher",
-      description: "Dr. Jane Smith is an AI researcher with over 15 years of experience in deep learning and neural networks. She has published numerous papers and is a keynote speaker at various AI conferences worldwide.",
-      image: "/judges/judge1.jpg"
-  },
-  {
-      name: "John Doe",
+      name: "Prof. Jack Wong",
       title: "Cloud Solutions Architect",
       description: "John Doe is a Cloud Solutions Architect who specializes in designing scalable cloud infrastructures. With a decade of experience in the industry, John has led multiple successful cloud migration projects.",
-      image: "/judges/judge3.jpg"
+      image: "/judges/judge3.png"
   },
   {
-      name: "Dr. Emily Zhang",
+    name: "Dr. Jane Smith",
+    title: "AI Researcher",
+    description: "Dr. Jane Smith is an AI researcher with over 15 years of experience in deep learning and neural networks. She has published numerous papers and is a keynote speaker at various AI conferences worldwide.",
+    image: "/judges/judge1.jpg"
+},
+  {
+      name: "Prof. Saif Al-Zaahwi",
       title: "Backend Developer",
       description: "Dr. Emily Zhang is a renowned backend developer known for her expertise in building robust backend systems. She has been a guest lecturer at several prestigious universities and is passionate about teaching and mentoring.",
       image: "/judges/judge2.png"
